1dfb1f488078bb570bb48c8d2c280f7c33b552e1
Shashwat A <shashwatanand07@gmail.com>
2026-06-09 23:36:19 +0530

    converted js to ts code

--- apps/frontend/src/routes/+page.svelte
+++ apps/frontend/src/routes/+page.svelte
@@ -1,18 +1,6 @@
-<script>
+<script lang="ts">
     import MainContainer from '$lib/components/ChatWindow.svelte'
-
-    let messages = [
-    {
-        id: 1,
-        role: "user",
-        content: "Hello"
-    },
-    {
-        id: 2,
-        role: "assistant",
-        content: "Hi! How can I help you?"
-    }
-];
+    
 </script>
 
 <MainContainer />
